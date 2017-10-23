59cb3ade9db0f43d00f97b3d3bd782e9e02eab14
Charlie Jackson <charlie@charliejackson.com>
2017-10-23 18:34:01 +0100

    Switch images every 5 seconds

--- src/components/SlideShow/SlideShow.render.jsx
+++ src/components/SlideShow/SlideShow.render.jsx
@@ -1,10 +1,52 @@
 import React, { PureComponent } from 'react';
 import Slide from 'components/Slide/Slide.render';
+import PropTypes from 'prop-types';
 
 class SlideShowRender extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    const slideIterator = 0;
+    const slide = props.slides[slideIterator] || null;
+
+    this.state = { slideIterator, slide };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      let slideIterator = this.state.slideIterator + 1;
+      let slide = this.props.slides[slideIterator];
+
+      if (!slide) {
+        slideIterator = 0;
+        slide = this.props.slides[slideIterator];
+      }
+
+      if (!slide) {
+        slide = null;
+      }
+
+      this.setState({ slideIterator, slide });
+    }, 5000);
+  }
+
+  componentWillUnmount() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
   render() {
-    return <div>{this.props.slides.map(image => <Slide image={image} />)}</div>;
+    if (!this.state.slide) {
+      return null;
+    }
+
+    return <Slide image={this.state.slide} />;
   }
 }
+
+SlideShowRender.propTypes = {
+  slides: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default SlideShowRender;
