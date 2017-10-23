98a49c9549b0982f6e1f02a2b7a3e938bd53e174
Charlie Jackson <charlie@charliejackson.com>
2017-10-23 15:46:49 +0100

    Added ejs as a template language

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import { compile } from 'ejs';
+import { readFileSync } from 'fs-extra';
+import { join } from 'path';
 
 const app = express();
 
+const str = readFileSync(join(__dirname, 'index.html'), { encoding: 'utf8' });
+const template = compile(str);
+const html = template();
+
 app.get('*', (req, res) => {
-  res.send('Hello World!');
+  res.send(html);
 });
 
 app.listen(3000, () => {
+  // eslint-disable-next-line no-console
   console.log('Example app listening on port 3000!');
 });
