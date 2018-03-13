50fdb03cf819a69506acb70d3ab5c19ef57562ac
Charlie Jackson <charlie@charliejackson.com>
2018-03-13 21:31:14 +0000

    Show instagram photos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,83 @@
 // @flow
 
 import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      images: null,
+      state: "REQUESTED",
+      message: null,
+      displayIndex: 0
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://www.instagram.com/charlie_a_jackson/?__a=1")
+      .then(res => res.json())
+      .then(json =>
+        json.graphql.user.edge_owner_to_timeline_media.edges.map(
+          edge => edge.node.display_url
+        )
+      )
+      .then(images => this.setState({ images, state: "SUCCEEDED" }))
+      .then(() => {
+        setInterval(() => {
+          let displayIndex = this.state.displayIndex + 1;
+
+          if (displayIndex >= this.state.images.length) {
+            displayIndex = 0;
+          }
+
+          this.setState({ displayIndex });
+        }, 3000);
+      })
+      .catch(err => {
+        console.error(err);
+
+        this.setState({
+          state: "FAILED",
+          message: err.message || "Undefined error"
+        });
+      });
+  }
+
   render() {
-    return <h1>Hello</h1>;
+    if (this.state.images && this.state.images.length) {
+      return (
+        <div
+          className="app"
+          style={{
+            width: `${this.state.images.length * 100}vw`,
+            marginLeft: `-${this.state.displayIndex * 100}vw`
+          }}
+        >
+          <div className="app__list">
+            {this.state.images.map(image => (
+              <div
+                key={image}
+                className="app__listitem"
+                style={{ backgroundImage: `url('${image}')` }}
+              />
+            ))}
+          </div>
+        </div>
+      );
+    }
+
+    switch (this.state.state) {
+      case "REQUESTED":
+        return <h1>Loading</h1>;
+
+      case "FAILED":
+        return <h1>Error: {this.state.message}</h1>;
+
+      default:
+        return <h1>Success but not images</h1>;
+    }
   }
 }
 
